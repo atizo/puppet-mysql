97a055d3108e5ae88b928a9a7dc6a3fa072bba87
Marcel Haerry <haerry@puzzle.ch>
2008-11-03 16:27:14 +0100

    the restriction is for the part before the @

--- plugins/puppet/type/mysql_user.rb
+++ plugins/puppet/type/mysql_user.rb
@@ -6,7 +6,7 @@ Puppet::Type.newtype(:mysql_user) do
         desc "The name of the user. This uses the 'username@hostname' form."
 
         validate do |value|
-            if value.size > 16
+            if value.split('@').first.size > 16
                 raise ArgumentError,
                     "MySQL usernames are limited to a maximum of 16 characters"
             else
